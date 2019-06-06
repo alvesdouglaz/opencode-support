cb7bc8ff984046ae26a9490d7136593d65345dca
Douglas Alves <alves.douglaz@gmail.com>
2019-06-06 18:24:45 -0300

    creatign ACL Entities Supports

---
+++ Auth/Access/RoleAcl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The Opencode Support package
+ *
+ * @package     Opencode/Support
+ * @copyright   2019 (c) Douglas Alves
+ * @author      Douglas Alves <https://github.com/alvesdouglaz/>
+ * @link        https://github.com/alvesdouglaz/opencode-Support
+ * @license     https://github.com/alvesdouglaz/opencode-Support/blob/master/LICENSE Apache-2.0
+ * @version     1.0.0
+ */
+
+namespace Opencode\Support\Auth\Access
+{
+
+    use Opencode\Contracts\Auth\Access\RoleAcl as RoleAclContract;
+
+    /**
+     * Role Support to Acl
+     *
+     * @package     Opencode/Support
+     * @author      Douglas Alves <https://github.com/alvesdouglaz/>
+     * @since       1.0.0
+     */
+    abstract class RoleAcl implements RoleAclContract
+    {
+        private $name;
+    }
+}
